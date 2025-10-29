cd1c9a487c96ad8021c87be94c6a295b4ed36d26
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2025-10-29 17:27:11 -0300

    ADD: Proyecto backend de cowork-app

---
+++ cowork-app_backend/src/main/java/cl/usach/tbd/coworkapp_backend/CoworkAppBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.tbd.coworkapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoworkAppBackendApplication.class, args);
+    }
+
+}
---
+++ cowork-app_backend/src/test/java/cl/usach/tbd/coworkapp_backend/CoworkAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.tbd.coworkapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
